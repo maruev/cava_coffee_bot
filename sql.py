0750719d807a535c8c4f27c523a70c9593f412ee
Alex <maruevski123@gmail.com>
2022-11-05 11:57:47 +0300

    Добавил функция для INSERT запросов

--- sql.py
+++ sql.py
@@ -1,9 +1,20 @@
 import sqlite3
+from typing import Optional
 
-def get_sql_response(request: str) -> list[tuple]:
-    connect = sqlite3.connect('db.db')
+def sql_select(querry: str) -> list[tuple]:
+    connect = sqlite3.connect('/mnt/d/db.db')
     cursor = connect.cursor()
-    cursor.execute(request)
+    cursor.execute(querry)
     data = cursor.fetchall()
     return data
 
+def sql_insert(querry: str, values: Optional[list] = None) -> None:
+    connect = sqlite3.connect('/mnt/d/db.db')
+    cursor = connect.cursor()
+    if values == None:
+        cursor.execute(querry)
+    else:
+        cursor.execute(querry, values)
+    cursor.close()
+    connect.commit()
+    connect.close()
